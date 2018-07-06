91cbf38816582edfe9538c461c53e753b05f8bd9
Willian Gabriel Cerqueira da Rocha <36766342+wgrocha@users.noreply.github.com>
2018-07-06 09:02:10 -0300

    Update ArduinoInlineAssembly.ino

--- ArduinoInlineAssembly.ino
+++ ArduinoInlineAssembly.ino
@@ -4,8 +4,8 @@ Just a demonstration on how to use inline assembly code
 with arduino - The board used was Uno-R3
 
 For more information take a look at:
-<http://www.nongnu.org/avr-libc/user-manual/inline_asm.html>
-<http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-42735-8-bit-AVR-Microcontroller-ATmega328-328P_Datasheet.pdf>
+http://www.nongnu.org/avr-libc/user-manual/inline_asm.html
+http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-42735-8-bit-AVR-Microcontroller-ATmega328-328P_Datasheet.pdf
 
 This is free and unencumbered software released.
 Anyone is free to copy, modify, publish, use, compile, sell, or
